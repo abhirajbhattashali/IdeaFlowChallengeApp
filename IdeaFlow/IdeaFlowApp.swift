550ddd372b9f5778f642825769fe745bb9726c2d
Abhiraj Bhattashali <abhirajbhattashali@gmail.com>
2023-08-05 23:03:54 +0530

    Initial Commit

---
+++ IdeaFlow/IdeaFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  IdeaFlowApp.swift
+//  IdeaFlow
+//
+//  Created by Abhiraj on 05/08/23.
+//
+
+import SwiftUI
+
+@main
+struct IdeaFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
